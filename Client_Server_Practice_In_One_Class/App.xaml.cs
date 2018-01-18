459e352fabad4c61bb94711d2143acecfc9e8341
Ian <ianburrows.sa@gmail.com>
2018-01-18 09:55:10 +0100

    Add project files.

---
+++ Client_Server_Practice_In_One_Class/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Client_Server_Practice_In_One_Class
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
